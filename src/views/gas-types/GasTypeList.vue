bdfc2d5746621e8392d9dbbfe8e64cbb70322494
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-07-24 16:38:31 -0700

    Add gas type list view

---
+++ src/views/gas-types/GasTypeList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tw-w-full">
+    <v-text-field
+      v-model="searchFilter"
+      class="tw-mb-10"
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+      variant="solo"
+      density="comfortable"
+      @keyup.enter="onEnterSearch"
+    >
+    </v-text-field>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, reactive, ref, watch, Ref } from 'vue';
+import { VDataTableServer } from 'vuetify/lib/labs/components.mjs';
+import { supabase } from '@/supabase';
+</script>
