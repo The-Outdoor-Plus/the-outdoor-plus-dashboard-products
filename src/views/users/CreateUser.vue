ab90a0ea75574db3d9e1050cda3ac0903872c781
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-07-24 15:18:18 -0700

    Added create user view

---
+++ src/views/users/CreateUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <user-form new></user-form>
+  </div>
+</template>
+<script lang="ts" setup>
+import UserForm from '@/components/UserForm.vue';
+import { useAppStore } from '@/store/app';
+import { onMounted } from 'vue';
+
+const store = useAppStore();
+
+onMounted(() => {
+  store.pageTitle = 'Users - New';
+})
+</script>
