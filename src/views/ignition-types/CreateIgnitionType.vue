15b4823979204afd4e603309350a4d94f583e866
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-07-25 14:21:52 -0700

    Added ignition type form to create

---
+++ src/views/ignition-types/CreateIgnitionType.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ignition-type-form new></ignition-type-form>
+  </div>
+</template>
+<script lang="ts" setup>
+import IgnitionTypeForm from '@/components/IgnitionTypeForm.vue';
+import { useAppStore } from '@/store/app';
+import { onMounted } from 'vue';
+
+const store = useAppStore();
+
+onMounted(() => {
+  store.pageTitle = 'Ignition Types - New';
+});
+</script>
