cab1ddf1936e48d0b1bf185eb5bbda2a4a79fec5
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-07-26 17:04:51 -0700

    Added color create view

---
+++ src/views/colors/CreateColor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <color-form new></color-form>
+  </div>
+</template>
+<script lang="ts" setup>
+import ColorForm from '@/components/ColorForm.vue';
+import { useAppStore } from '@/store/app';
+import { onMounted } from 'vue';
+
+const store = useAppStore();
+
+onMounted(() => {
+  store.pageTitle = 'Colors - New';
+});
+</script>
