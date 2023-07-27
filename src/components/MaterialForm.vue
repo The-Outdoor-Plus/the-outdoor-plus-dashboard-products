c257502ff5c05e8d2ac686e64ee8d985e7d59a6b
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-07-27 09:44:47 -0700

    Added image url text field and colors list

--- src/components/MaterialForm.vue
+++ src/components/MaterialForm.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="tw-w-full">
-    <v-card class="py-12 px-10" :loading="isLoading">
+    <v-card 
+      class="py-12 px-10" 
+      :class="showColors ? 'tw-mb-10' : 'tw-mb-60'"
+      :loading="isLoading"
+    >
       <v-btn
         class="-tw-mt-6 tw-mb-6 -tw-ml-4" 
         icon="mdi-arrow-left" 
@@ -54,6 +58,57 @@
             </span>
           </div>
         </div>
+        <template v-if="props.new || props.edit || (props.readonly && props.material?.image_url && (props?.material?.color?.length || 0 <= 0))">
+          <v-divider class="border-opacity-100 tw-my-6"></v-divider>
+          <div class="tw-w-full tw-flex tw-flex-col lg:tw-flex-row">
+            <div class="tw-w-full lg:tw-w-3/12">
+              <h3 class="tw-text-base tw-font-semibold tw-mt-1">Image Url Link</h3>
+              <span class="tw-text-sm tw-text-gray-500 tw-mt-1">
+                In order to make the link work, when you click on the button "copy link" from the dropbox,
+                you'll get a link that ends with <span class="tw-italic tw-font-semibold">?dl=0</span> . You'll need to replace
+                that with <span class="tw-italic tw-font-semibold">?raw=1</span>
+              </span>
+            </div>
+            <div class="tw-w-full tw-mt-3 lg:tw-mt-0 lg:tw-w-8/12 xl:tw-w-5/12">
+              <v-textarea
+                v-model="imageUrl.value.value"
+                variant="outlined"
+                density="compact"
+                name="Image"
+                placeholder="https://link-to-dropbox.com?raw=1"
+                :error-messages="imageUrl.errorMessage.value"
+                :readonly="readonly"
+              >
+              </v-textarea>
+            </div>
+          </div>
+        </template>
+        <template v-if="props.readonly && props.material?.image_url && (props?.material?.color?.length || 0 <= 0)">
+          <v-divider class="border-opacity-100 tw-my-6"></v-divider>
+          <div class="tw-w-full tw-flex tw-flex-col lg:tw-flex-row">
+            <div class="tw-w-full lg:tw-w-3/12">
+              <h3 class="tw-text-base tw-font-semibold tw-mt-1">Color Preview</h3>
+              <span v-if="!props.readonly" class="tw-text-sm tw-text-gray-500 tw-mt-1">
+              </span>
+            </div>
+            <div class="tw-w-full tw-mt-3 tw-mb-10 lg:tw-mt-0 lg:tw-w-7/12 xl:tw-w-4/12">
+              <v-img
+                width="250"
+                :lazy-src="material?.image_url || ''"
+                :src="material?.image_url || ''"
+              >
+                <template v-slot:placeholder>
+                  <div class="d-flex align-center justify-center fill-height">
+                    <v-progress-circular
+                      color="grey-lighten-4"
+                      indeterminate
+                    ></v-progress-circular>
+                  </div>
+                </template>
+              </v-img>
+            </div>
+          </div>
+        </template>
         <v-divider class="border-opacity-100 tw-mb-6 tw-mt-1"></v-divider>
         <div class="tw-w-full">
           <v-spacer></v-spacer>
@@ -64,6 +119,41 @@
           >Submit</v-btn>
         </div>
       </form>
+    </v-card>
+    <v-card 
+      v-if="showColors"
+      class="py-12 px-10 tw-mb-32"
+      :loading="isLoading"
+    >
+      <div class="tw-w-full">
+        <h1 class="tw-text-base tw-font-semibold">Colors</h1>
+        <div class="tw-text-sm">Preview list of colors associated to {{ material?.name }}</div>
+      </div>
+      <v-divider class="border-opacity-100 tw-my-6"></v-divider>
+      <div class="tw-w-full tw-flex tw-flex-wrap tw-mx-auto tw-justify-center">
+        <div 
+          v-for="(color, i) in material?.color"
+          :key="i"
+          class="tw-w-2/12 tw-flex tw-flex-col tw-justify-center tw-items-center tw-py-6"
+        >
+          <v-img
+            width="160"
+            :lazy-src="color?.image_url || ''"
+            :src="color?.image_url || ''"
+          >
+            <template v-slot:placeholder>
+              <div class="d-flex align-center justify-center fill-height">
+                <v-progress-circular
+                  color="grey-lighten-4"
+                  indeterminate
+                ></v-progress-circular>
+              </div>
+            </template>
+          </v-img>
+          <span class="tw-text-lg tw-font-bold tw-mt-3">{{ color?.name || '' }}</span>
+          <span class="tw-text-base tw-uppercase -tw-mt-1">-{{ color?.slug || '' }}</span>
+        </div>
+      </div>
     </v-card>
   </div>
 </template>
@@ -82,10 +172,19 @@ import { useRouter } from 'vue-router';
  * 
  */
 
+interface Color {
+  id?: number;
+  name?: string;
+  image_url?: string;
+  slug?: string;
+}
+
 interface Material {
   id?: number;
   name: string;
   slug?: string;
+  image_url?: string | null;
+  color?: Color[];
 }
 
 interface Props {
@@ -134,6 +233,10 @@ const subtitle = computed(() => {
   return '';
 });
 
+const showColors = computed(() => {
+  return props.readonly && props.material?.color?.length;
+})
+
 /**
  * 
  * Slug definitions
@@ -168,17 +271,20 @@ const { handleSubmit } = useForm({
     yup.object({
       name: yup.string().min(2).required(),
       slug: yup.string(),
+      image: yup.string(),
     })
   ),
 });
 
 const name = useField<string>('name');
 const slug = useField<string>('slug');
+const imageUrl = useField<string>('image');
 
 const fillMaterialInformation = () => {
   if (props.edit || props.readonly) {
     name.value.value = props.material?.name || '';
     slug.value.value = props.material?.slug || '';
+    imageUrl.value.value = props.material?.image_url || '';
   }
 }
 
@@ -199,11 +305,9 @@ watch(
  * 
  */
 
-const handleCreate = async (values: Material) => {
+const handleCreate = async (form: Material) => {
   try {
     isLoading.value = true;
-    const form = JSON.parse(JSON.stringify(values));
-    if (!form.slug) form.slug = slugify(form.name);
     const { data: material , error } = await supabase
       .from('material')
       .insert(form)
@@ -228,11 +332,9 @@ const handleCreate = async (values: Material) => {
   }
 }
 
-const handleUpdate = async (values: Material) => {
+const handleUpdate = async (form: Material) => {
   try {
     isLoading.value = true;
-    const form = JSON.parse(JSON.stringify(values));
-    if (!form.slug) form.slug = slugify(form.name);
     const { data: material, error } = await supabase
       .from('material')
       .update(form)
@@ -242,6 +344,7 @@ const handleUpdate = async (values: Material) => {
     if(material.length) {
       name.value.value = material[0].name;
       slug.value.value = material[0].slug;
+      imageUrl.value.value = material[0].image_url;
     }
     notify({
       title: 'Material updated successfully',
@@ -263,10 +366,14 @@ const handleUpdate = async (values: Material) => {
 }
 
 const submit = handleSubmit(async (values) => {
+  const form = JSON.parse(JSON.stringify(values));
+  if (!form.slug) form.slug = slugify(form.name);
+  form.image_url = form.image;
+  delete form.image;
   if (props.new) {
-    await handleCreate(values);
+    await handleCreate(form);
   } else if (props.edit) {
-    await handleUpdate(values);
+    await handleUpdate(form);
   }
 })
 
