109b8b047ab94ee9fe10b3d9a077397536ec4544
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-08-08 16:55:02 -0700

    Added variants list to view product

--- src/views/products/ViewProduct.vue
+++ src/views/products/ViewProduct.vue
@@ -5,20 +5,109 @@
       :product="product"
       readonly
     ></product-form>
+    <div class="tw-w-full tw-my-16 rounded">
+      <v-data-table-server
+        v-model:items-per-page="itemsPerPage"
+        :key="`${route.params.id}`"
+        :headers="(headers as [])"
+        :items-length="totalItems"
+        :items="data.serverItems"
+        :loading="loading"
+        class="elevation-1"
+        rounded
+        item-value="first_name"
+        @update:options="loadItems"
+      >
+        <template v-slot:top>
+          <v-toolbar flat compact color="green-darken-2" class="rounded-t">
+            <v-toolbar-title>Child products (Variants) </v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-btn color="white" dark :to="`/products/new?parent_id=${route.params.id}&relation=child`" exact>New Variant<v-icon icon="mdi-plus" class="ml-2"></v-icon></v-btn>
+          </v-toolbar>
+          <v-dialog v-model="dialogDelete" max-width="600px">
+            <v-card class="pt-4 pb-3" :loading="deleteLoading">
+              <v-card-title class="text-h5">Are you sure you want to delelete this variant?</v-card-title>
+              <v-card-text>
+                Variant <strong>{{ itemToDelete?.name }}</strong> will be deleted.
+                This action cannot be reversed. Are you sure you want to continue?
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="closeDialogDelete">Cancel</v-btn>
+                <v-btn color="red-darken-1" variant="text" @click="deleteItemConfirm">Delete</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </template>
+        <!-- eslint-disable-next-line vue/valid-v-slot -->
+        <template v-slot:item.published="{ item }">
+          <v-icon v-if="item.raw.published" icon="mdi-checkbox-marked-circle-outline" color="green"></v-icon>
+          <v-icon v-else icon="mdi-close-circle-outline" color="red"></v-icon>
+        </template>
+        <!-- eslint-disable-next-line vue/valid-v-slot -->
+        <template v-slot:item.enabled="{ item }">
+          <v-icon v-if="item.raw.enabled" icon="mdi-checkbox-marked-circle-outline" color="green"></v-icon>
+          <v-icon v-else icon="mdi-close-circle-outline" color="red"></v-icon>
+        </template>
+        <!-- eslint-disable-next-line vue/valid-v-slot -->
+        <template v-slot:item.actions="{ item }">
+          <div class="tw-flex tw-flex-nowrap tw-justify-end -tw-mr-1.5">
+            <v-btn
+              size="small"
+              icon="mdi-eye"
+              variant="text"
+              :to="`/products/${item.raw.id}`"
+            ></v-btn>
+            <v-btn
+              size="small"
+              icon="mdi-pencil"
+              variant="text"
+              :to="`/products/edit/${item.raw.id}`"
+            ></v-btn>
+            <v-btn
+              size="small"
+              @click="deleteItem((item.raw as Columns))"
+              icon="mdi-delete"
+              variant="text"
+            ></v-btn>
+          </div>
+        </template>
+      </v-data-table-server>
+    </div> 
   </div>
 </template>
 <script lang="ts" setup>
 import ProductForm from '@/components/ProductForm.vue';
+import { VDataTableServer } from 'vuetify/lib/labs/components.mjs';
 import { useAppStore } from '@/store/app';
 import { supabase } from '@/supabase';
 import { useNotification } from '@kyvg/vue3-notification';
-import { onMounted, ref, Ref } from 'vue';
+import { onMounted, reactive, ref, Ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import { Product } from '@/types/product';
+import { usePagination } from '@/utils';
 
-interface Product {
-  id?: number;
+interface Data {
+  serverItems: Product[];
+}
+
+interface Columns {
+  actions: null | undefined;
+  id: number;
   name: string;
-  slug?: string;
+  sku: string;
+  collection: string;
+  category: string;
+  material: string;
+  published: boolean;
+  enabled: boolean;
+}
+
+interface TableOptions {
+  page: number;
+  itemsPerPage: number;
+  sortBy: any;
 }
 
 const store = useAppStore();
@@ -29,11 +118,9 @@ const loading = ref(false);
 const product: Ref<Product> = ref<Product>({
   id: 0,
   name: '',
-  slug: '',
 });
 
-onMounted(async () => {
-  store.pageTitle = 'Product - View';
+const loadData = async () => {
   try {
     loading.value = true;
     const { data, error } = await supabase
@@ -53,5 +140,142 @@ onMounted(async () => {
   } finally {
     loading.value = false;
   }
+}
+
+const headers = ref([
+  {
+    title: 'ID',
+    align: 'start',
+    sortable: false,
+    key: 'id',
+  },
+  { title: 'Name', align: 'end', key: 'name', width: '250px' },
+  { title: 'SKU', align: 'end', key: 'sku', width: '300px' },
+  { title: 'Collection', align: 'end', key: 'collection', width: '150px' },
+  { title: 'Category', align: 'end', key: 'category', width: '220px' },
+  { title: 'Material', align: 'end', key: 'material', width: '220px' },
+  { title: 'Published', align: 'end', key: 'published' },
+  { title: 'Enabled', align: 'end', key: 'enabled' },
+  { title: 'Actions', key: 'actions', sortable: false, align: 'end' },
+]);
+const data: Data = reactive({
+  serverItems: [],
 });
+const itemsPerPage = ref(40);
+const totalItems = ref(40);
+
+/**
+ * 
+ * Dialog Delete Section
+ * 
+ **/
+
+const itemToDelete: Ref<Columns | null> = ref(null);
+const deleteLoading = ref(false);
+const dialogDelete = ref(false);
+
+watch(dialogDelete, (value) => {
+  if (!value) {
+    itemToDelete.value = null;
+  }
+});
+watch(route, async (value) => {
+  if (route.params.id)
+    await loadData();
+});
+
+const deleteItem = (item: Columns) => {
+  dialogDelete.value = true;
+  itemToDelete.value = item;
+}
+
+const closeDialogDelete = () => {
+  dialogDelete.value = false;
+  itemToDelete.value = null;
+}
+
+const deleteItemConfirm = async () => {
+  try {
+    deleteLoading.value = true;
+    const { error } = await supabase.from('product').delete()
+      .eq('id', itemToDelete?.value?.id);
+    if (error) throw error;
+    notify({
+      type: 'success',
+      title: 'Product deleted successfully',
+      duration: 6000,
+    });
+    data.serverItems = data.serverItems.filter((item) => item.id !== itemToDelete?.value?.id);
+  } catch (e: any) {
+    console.error(e);
+    notify({
+      title: 'Error deleting product.',
+      text: e?.message || 'An error ocurred trying to delete product. Please contact TOP Support.',
+      type: 'error',
+      duration: 6000,
+    });
+  } finally {
+    deleteLoading.value = false;
+    dialogDelete.value = false;
+    itemToDelete.value = null;
+  }
+}
+
+/**
+ * 
+ * Search
+ * 
+ */
+const search = ref('');
+const searchFilter = ref('');
+
+const onEnterSearch = () => {
+  search.value = String(Date.now());
+}
+
+watch(searchFilter, (searchValue) => {
+  if(!searchValue) {
+    search.value = String(Date.now());
+  }
+});
+
+onMounted(async () => {
+  store.pageTitle = 'Product - View';
+  await loadData();
+});
+
+const loadItems = async ({ page, itemsPerPage, sortBy }: TableOptions) => {
+  try {
+    loading.value = true;
+    const { from, to } = usePagination(page -1, itemsPerPage);
+    const { data: products, error, count } = await supabase
+      .from('product')
+      .select('id, name, sku, relation, enabled, published, collection(name), category(name), material:material_id(name)', { count: 'exact' })
+      .eq(`relation`, 'CHILD')
+      .eq(`parent_id`, +route.params.id)
+      .order(sortBy?.[0]?.key || 'name', {
+        ascending: sortBy?.[0]?.order === 'desc' ? false : true
+      })
+      .range(from, to);
+    if (error) throw error;
+    const transformedProducts = products.map(product => ({
+      ...product,
+      category: (product.category as any)?.name || '', 
+      material: (product.material as any)?.name || '',
+      collection: (product.collection as any)?.name || '',
+    }));  
+    data.serverItems = transformedProducts || [];
+    totalItems.value = count || 0;
+  } catch (e: any) {
+    console.error(e);
+    notify({
+      title: 'Error loading variants.',
+      text: e?.message || 'An error ocurred trying to load variants. Please contact TOP Support.',
+      type: 'error',
+      duration: 6000,
+    });
+  } finally {
+    loading.value = false;
+  }
+}
 </script>
