9157bf3efa7c930dcaa0b28c4748b47f8d30bd3a
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-02-22 09:42:45 -0800

    Create attribute form

---
+++ src/views/attributes/CreateAttribute.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <attributes-form new></attributes-form>
+  </div>
+</template>
+<script lang="ts" setup>
+import AttributesForm from '@/components/AttributesForm.vue';
+import { useAppStore } from '@/store/app';
+import { onMounted } from 'vue';
+
+const store = useAppStore();
+
+onMounted(() => {
+  store.pageTitle = 'Attributes - New';
+});
+</script>
