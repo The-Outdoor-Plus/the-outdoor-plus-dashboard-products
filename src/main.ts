1059b6b14619fb5bc12487082b5d78cd6acbef5e
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-04-11 16:06:14 -0700

    Add Sentry

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,25 @@ import { createApp } from 'vue';
 
 // Plugins
 import { registerPlugins } from './plugins';
+import * as Sentry from '@sentry/vue';
 
 const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", import.meta.env.VITE_SUPABASE_URL],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 registerPlugins(app);
 
