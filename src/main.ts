0af9b197072efdb1087ef2b75edd5a77a4588831
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-04-12 09:19:44 -0700

    Manually capture exceptions for sentry

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,16 @@ import { createApp } from 'vue';
 
 // Plugins
 import { registerPlugins } from './plugins';
+import * as Sentry from '@sentry/vue';
 
 const app = createApp(App);
 
 registerPlugins(app);
+
+app.config.errorHandler = (err, vm, info) => {
+  if (import.meta.env.VITE_VERCEL_ENV) {
+    Sentry.captureException(err);
+  }
+}
 
 app.mount('#app');
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -37,5 +37,5 @@ export function registerPlugins (app: App) {
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  })
+  });
 }
