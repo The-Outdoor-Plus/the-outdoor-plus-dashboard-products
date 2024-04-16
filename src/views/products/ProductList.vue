df6fcf6d6f33edad7f6f469a260c25b8342b7b39
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-04-16 15:13:46 -0700

    Remove published from products list

--- src/views/products/ProductList.vue
+++ src/views/products/ProductList.vue
@@ -179,7 +179,6 @@ const headers = ref([
   { title: 'Name', align: 'end', key: 'name', width: '250px' },
   { title: 'SKU', align: 'end', key: 'sku', width: '300px' },
   { title: 'Collection', align: 'end', key: 'collection', width: '150px' },
-  { title: 'Published', align: 'end', key: 'published' },
   { title: 'Enabled', align: 'end', key: 'enabled' },
   { title: 'Actions', key: 'actions', sortable: false, align: 'end' },
 ]);
