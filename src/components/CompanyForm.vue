36ca83606f4cf5a03ab59ab0b08c33b369fa093d
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-08-09 11:21:00 -0700

    Add option to upload image to Color, Company and Material

--- src/components/CompanyForm.vue
+++ src/components/CompanyForm.vue
@@ -80,29 +80,74 @@
             ></v-select>
           </div>
         </div>
-        <v-divider class="border-opacity-100 tw-my-6"></v-divider>
-        <div class="tw-w-full tw-flex tw-flex-col lg:tw-flex-row">
-          <div class="tw-w-full lg:tw-w-3/12">
-            <h3 class="tw-text-base tw-font-semibold tw-mt-1">Logo URL</h3>
-            <span class="tw-text-sm tw-text-gray-500 tw-mt-1">
-              In order to make the link work, when you click on the button "copy link" from the dropbox,
-              you'll get a link that ends with <span class="tw-italic tw-font-semibold">?dl=0</span> . You'll need to replace
-              that with <span class="tw-italic tw-font-semibold">?raw=1</span>
-            </span>
+        <template v-if="props.new || props.edit">
+          <v-divider class="border-opacity-100 tw-my-6"></v-divider>
+          <div class="tw-w-full tw-flex tw-flex-col lg:tw-flex-row">
+            <div class="tw-w-full lg:tw-w-3/12">
+              <h3 class="tw-text-base tw-font-semibold tw-mt-1">Logo</h3>
+              <span class="tw-text-sm tw-text-gray-500 tw-mt-1">
+              </span>
+            </div>
+            <div class="tw-w-full tw-mt-3 lg:tw-mt-0 lg:tw-w-8/12 xl:tw-w-5/12">
+              <v-file-input
+                label="Logo Image"
+                variant="outlined"
+                density="compact"
+                name="Image"
+                show-size
+                :readonly="readonly"
+                @change="onFileChange($event)"
+                @click:clear="clearImage"
+              ></v-file-input>
+              <div>
+                <div v-show="imagePreviewURL" class="tw-text-base tw-font-semibold tw-mb-4">Upload Preview: </div>
+                <v-img
+                    v-show="imagePreviewURL"
+                    width="250"
+                    :src="imagePreviewURL || ''"
+                  >
+                </v-img>
+              </div>
+              <v-expansion-panels class="mb-5 mt-8">
+                <v-expansion-panel>
+                  <v-expansion-panel-title disable-icon-rotate>
+                    Advanced Options
+                    <template v-slot:actions>
+                      <v-icon color="orange" icon="mdi-alert">
+                      </v-icon>
+                    </template>
+                  </v-expansion-panel-title>
+                  <v-expansion-panel-text>
+                    <h3 class="tw-text-base tw-font-semibold tw-mt-1">Logo Url Link</h3>
+                    <span class="tw-text-sm tw-text-gray-700 tw-mt-1">
+                      Preferrably, you need to upload a new image. In case that the image is already present in the AWS S3 Bucket,
+                      you can copy the path of the image along with our cloudfront domain (https://assets.theoutdoorplus.com/). E.g. If the image is uploaded to "companies/top.png",
+                      the link is going to be: https://assets.theoutdoorplus.com/companies/top.png
+                    </span>
+                    <br>
+                    <br>
+                    <span class="tw-text-sm tw-text-gray-700 tw-mt-1">
+                      <b>If Link is from dropbox (Not recommended): </b>In order to make the link work, when you click on the button "copy link" from the dropbox,
+                      you'll get a link that ends with <span class="tw-italic tw-font-semibold">?dl=0</span> . You'll need to replace
+                      that with <span class="tw-italic tw-font-semibold">?raw=1</span>
+                    </span>
+                    <v-textarea
+                      v-model="logoUrl.value.value"
+                      class="mt-6"
+                      variant="outlined"
+                      density="compact"
+                      name="Image"
+                      placeholder="https://link-to-dropbox.com?raw=1"
+                      :error-messages="logoUrl.errorMessage.value"
+                      :readonly="readonly"
+                    >
+                    </v-textarea>
+                  </v-expansion-panel-text>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </div>
           </div>
-          <div class="tw-w-full tw-mt-3 lg:tw-mt-0 lg:tw-w-7/12 xl:tw-w-4/12">
-            <v-text-field
-              v-model="logoUrl.value.value"
-              variant="outlined"
-              density="compact"
-              name="logoURL"
-              placeholder="https://dropbox-link-to-log.com"
-              :error-messages="logoUrl.errorMessage.value"
-              :readonly="readonly"
-            >
-            </v-text-field>
-          </div>
-        </div>
+        </template>
         <v-divider class="border-opacity-100 tw-mb-6 tw-mt-1"></v-divider>
         <div class="tw-w-full">
           <v-spacer></v-spacer>
@@ -117,7 +162,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, computed, watch, onMounted } from 'vue';
+import { Ref, ref, computed, watch, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { useNotification } from '@kyvg/vue3-notification';
 import * as yup from 'yup';
@@ -256,6 +301,9 @@ const name = useField<string>('name');
 const slug = useField<string>('slug');
 const logoUrl = useField<string>('logo_url');
 const role = useField<string>('role');
+const imageFile: Ref<File | null> = ref<File | null>(null);
+const imagePreviewURL: Ref<string | null> = ref<string | null>(null);
+const oldImageUrl: Ref<string> = ref<string>('');
 
 const fillCompanyInformation = () => {
   if (props.edit || props.readonly) {
@@ -344,10 +392,114 @@ const handleUpdate = async (values: Company) => {
   }
 }
 
+const onFileChange = async (evt: Event) => {
+  const target = evt.target as HTMLInputElement;
+  if (target && target.files) {
+    imageFile.value = target.files[0];
+    imagePreviewURL.value = URL.createObjectURL(target.files[0]);
+  }
+}
 
+const clearImage = () => {
+  imageFile.value = null;
+  imagePreviewURL.value = null;
+}
+
+const handleUpload = async () => {
+  const formData = new FormData();
+  formData.append('file', imageFile.value as File);
+
+  let path = '/companies/';
+  const isDev = import.meta.env.DEV;
+  if (isDev) path = `/testing${path}`;
+  formData.append('path', path);
+
+  try {
+    isLoading.value = true;
+    const { data, error } = await supabase.functions.invoke('upload-to-s3', {
+      body: formData,
+    });
+    if (error) throw error;
+    if (data && data.fileUrl) {
+      notify({
+        title: 'Logo image uploaded successfully',
+        text: data?.message || 'File uplaoded successfully.',
+        type: 'success',
+        duration: 6000,
+      });
+      imagePreviewURL.value = null;
+      clearImage();
+      return data.fileUrl;
+    }
+  } catch (e: any) {
+    console.error(e);
+    notify({
+      title: 'Error updating logo image',
+      text: e?.message || 'An error ocurred trying to update the logo image. Please contact TOP support.',
+      type: 'error',
+      duration: 6000,
+    });
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+const deleteFile = async(imgUrl?: string) => {
+  try {
+    isLoading.value = true;
+    if (imgUrl && imgUrl !== '') {
+      const { data, error } = await supabase.functions.invoke('delete-from-s3', {
+        body: {
+          prevImageUrl: imgUrl,
+        },
+        method: 'DELETE',
+      });
+      if (error) throw error;
+      notify({
+        title: 'Old logo image deleted successfully',
+        text: data?.message || 'Previous file deleted.',
+        type: 'success',
+        duration: 6000,
+      });
+    }
+  } catch (e: any) {
+    notify({
+      title: 'Error deleting image from server.',
+      text: e?.message || 'An error ocurred trying to delete the old image from the server. Please contact TOP support.',
+      type: 'error',
+      duration: 6000,
+    });
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+const isImageUrlFromCloudFront = (imgUrl: string) => {
+  const cloudfrontDomain = import.meta.env.VITE_CLOUDFRONT_DOMAIN;
+  const parsedUrl = new URL(imgUrl);
+  return parsedUrl.hostname.endsWith(cloudfrontDomain);
+}
 
 const submit = handleSubmit(async (values) => {
-  if (props.new) await handleCreate(values);
-  else if (props.edit) await handleUpdate(values);
+  const form = JSON.parse(JSON.stringify(values));
+  if (props.new) {
+    if (imageFile.value) {
+      const imgUrl = await handleUpload();
+      if (imgUrl) form.logo_url = imgUrl;
+    }
+    await handleCreate(form);
+  } else if (props.edit) {
+    if (imageFile.value) {
+      oldImageUrl.value = logoUrl.value.value;
+      const imgUrl = await handleUpload();
+      if (imgUrl) {
+        form.logo_url = imgUrl;
+        if (isImageUrlFromCloudFront(oldImageUrl.value)) {
+          await deleteFile(oldImageUrl.value);
+        }
+      }
+    }
+    await handleUpdate(form);
+  }
 })
 </script>
